f94ca3db2e4319ce1fdaea18e76e6aace708158a
NeverGiveUp05 <tacong0903@gmail.com>
2025-04-01 00:48:46 +0700

    CRUD Danh mục

--- admin/models/AdminDanhMuc.php
+++ admin/models/AdminDanhMuc.php
@@ -1,18 +1,77 @@
-<?php 
+<?php
 
-class AdminDanhMuc {
+class AdminDanhMuc
+{
     public $conn;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->conn = connectDB();
     }
 
-    public function getAllDanhMuc() {
+    public function getAllDanhMuc()
+    {
         try {
             $sql = "SELECT * FROM danh_mucs";
             $stmt = $this->conn->prepare($sql);
             $stmt->execute();
             return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+    }
+
+    public function insertDanhMuc($ten_danh_muc, $mo_ta)
+    {
+        try {
+            $sql = "INSERT INTO danh_mucs (ten_danh_muc, mo_ta)
+                    VALUES (:ten_danh_muc, :mo_ta)";
+            $stmt = $this->conn->prepare($sql);
+            $stmt->execute([
+                ':ten_danh_muc' => $ten_danh_muc,
+                ':mo_ta' => $mo_ta
+            ]);
+            return true;
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+    }
+
+    public function getDetailDanhMuc($id)
+    {
+        try {
+            $sql = "SELECT * FROM danh_mucs WHERE id = :id";
+            $stmt = $this->conn->prepare($sql);
+            $stmt->execute([':id' => $id]);
+            return $stmt->fetch(PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+    }
+
+    public function updateDanhMuc($id, $ten_danh_muc, $mo_ta)
+    {
+        try {
+            $sql = "UPDATE danh_mucs SET ten_danh_muc = :ten_danh_muc, mo_ta = :mo_ta WHERE id = :id";
+            $stmt = $this->conn->prepare($sql);
+            $stmt->execute([
+                ':ten_danh_muc' => $ten_danh_muc,
+                ':mo_ta' => $mo_ta,
+                ':id' => $id
+            ]);
+            return true;
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+    }
+
+    public function destroyDanhMuc($id)
+    {
+        try {
+            $sql = "DELETE FROM danh_mucs WHERE id = :id";
+            $stmt = $this->conn->prepare($sql);
+            $stmt->execute([':id' => $id]);
+            return true;
         } catch (PDOException $e) {
             echo "Error: " . $e->getMessage();
         }
